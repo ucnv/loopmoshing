8b19f2ea47beec8d56553b73ef870575414c328a
ucnv <ucnvvv@gmail.com>
2014-05-22 16:22:49 +0900

    Tiny fixes

--- lib/loopmoshing/cli.rb
+++ lib/loopmoshing/cli.rb
@@ -1,6 +1,6 @@
 module Loopmoshing
   class Cli < Thor
-    desc 'generate', 'Make loopmoshing gif'
+    desc 'MOVIE [OUTFILE]', 'Generates loopmoshing image'
 
     def generate infile, outfile = nil
       outfile = outfile.nil? ? Pathname.new(infile).dirname.join('out.gif') : Pathname.new(outfile)
@@ -11,5 +11,7 @@ module Loopmoshing
         end
       end
     end
+
+    default_task :generate
   end
 end
